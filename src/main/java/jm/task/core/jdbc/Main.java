b14ef802ac74b0eef8cbff6b06100919c3540d11
TLOUII <kurama151@mail.ru>
2022-12-11 00:02:46 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getSessionFactory();
+        UserDao userDao = new UserDaoHibernateImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("Name1", "LastName1", (byte) 22);
+        userDao.saveUser("Name2", "LastName2", (byte) 22);
+        userDao.saveUser("Name3", "LastName3", (byte) 22);
+        userDao.saveUser("Name4", "LastName4", (byte) 22);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,18 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static final Logger log = Logger.getLogger(UserDaoHibernateImpl.class.getName());
+
     public UserDaoHibernateImpl() {
 
     }
@@ -13,30 +21,83 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createSQLQuery("CREATE TABLE IF NOT EXISTS " +
+                    "Users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(45) NOT NULL," +
+                    "last_name VARCHAR(45) NOT NULL," +
+                    "age TINYINT NULL) ");
+            transaction.commit();
+        } catch (Exception e) {
+            log.warning("При создании таблицы возникла ошибка");
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("DELETE User");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            log.warning("Таблица не удалена");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        User user = new User(name, lastName, age);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.save(user);
+            log.info("Добавлен пользователь  [name = " + name
+                    + ", lastName = " + lastName +
+                    ", age = " + age + "]");
+        } catch (Exception e) {
+            log.warning("Пользователь не удален, что-то введено не правильно у нового пользователя");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = new User();
+            user.setId(id);
+            session.delete(user);
+            transaction.commit();
+        } catch (Exception e) {
+            log.warning("Похоже был введен неверный id");
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("FROM User ");
+            userList = query.list();
+            transaction.commit();
+            userList.forEach(System.out::println);
+        } catch (Exception e) {
+            log.warning("С добавлением юзер в список возникли проблемы");
+        }
+
+
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("DELETE FROM User ");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            log.warning("При удалении пользователей возникла ошибка");
+        }
     }
 }
